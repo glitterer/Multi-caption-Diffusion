83554e2cd2b10b8397c9c2f1b878db3f2dc65333
RogDMK <rdemellokoch@gmail.com>
2024-04-29 11:00:41 -0400

    Sbatch fix

--- Diffusion-Models-pytorch-main/run_job.sh
+++ Diffusion-Models-pytorch-main/run_job.sh
@@ -8,7 +8,7 @@
 #SBATCH -e job_error_%x_%j.e
 #SBATCH -o job_out_%x_%j.o
 #SBATCH --nodes=1
-#SBATCH --exclude=gpu[1601-1605,1701-08,1801-02,1901-04,1905-06,2002-03]
+#SBATCH --exclude=gpu[1601-1605,1701-1708,1801-1802,1901-1904,1905-1906,2002-2003]
 #SBATCH --gpus-per-node=1
 
 source activate ~/miniconda3/envs/DLproject
