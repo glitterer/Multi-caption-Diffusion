56928e473e847b04166a506e9c90119f7b76f038
RogDMK <rdemellokoch@gmail.com>
2024-04-29 09:54:00 -0400

    Added node

--- Diffusion-Models-pytorch-main/run_job.sh
+++ Diffusion-Models-pytorch-main/run_job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name="ddpm_clip_caption"
-#SBATCH --partition=compute
-#SBATCH --mem=64G
+#SBATCH --job-name="ddpm_1_epoch_1_batch"
+#SBATCH --partition=gpus
+#SBATCH --mem=250G
 #SBATCH --cpus-per-task=8
 #SBATCH --time=2-10:00:00
 #SBATCH -e job_error_%x_%j.e
 #SBATCH -o job_out_%x_%j.o
 #SBATCH --nodes=1
+#SBATCH --nodelist=gpu[1701-1708]
 
 source activate ~/miniconda3/envs/DLproject
 cd ~/CSCI2470Project/Multi-caption-Diffusion/Diffusion-Models-pytorch-main
