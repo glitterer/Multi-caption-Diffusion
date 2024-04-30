813dafb1eb610e075295100343438601c34e9806
Seik Oh <seikoh@naver.com>
2024-04-29 21:52:25 -0400

    edit ssh file

--- Diffusion-Models-pytorch-main/run_job.sh
+++ Diffusion-Models-pytorch-main/run_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name="ddpm_1_epoch_1_batch"
+#SBATCH --job-name="ddpm_1eb"
 #SBATCH --partition=gpus
-#SBATCH --mem=120G
+#SBATCH --mem=124G
 #SBATCH --cpus-per-task=8
 #SBATCH --time=5-10:00:00
 #SBATCH -e job_error_%x_%j.e
@@ -11,8 +11,6 @@
 #SBATCH --exclude=gpu[1601-1605,1701-1708,1801-1802,1901-1904,1905-1906,2002-2003]
 #SBATCH --gpus-per-node=1
 
-modul load anaconda
 source activate ~/miniconda3/envs/DLproject
 cd ~/Multi-caption-Diffusion/Diffusion-Models-pytorch-main
-nvidia-smi
 python train.py
